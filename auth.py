efcd5ab535058051f1023b99d414323dd2b76292
Marco Antônio R. A. B. Ribeiro <marcoantoniorabr@gmail.com>
2025-11-04 09:34:57 -0300

    typo fix

--- auth.py
+++ auth.py
@@ -54,7 +54,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            "SELECT * FROM user WHERE id = ?", (user_id,)
+            "SELECT * FROM Users WHERE id = ?", (user_id,)
         ).fetchone()
         
 
