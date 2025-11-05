8dd991d9d46ea4c647cdf16ad4b18f388d00c1c9
Marco Antônio R. A. B. Ribeiro <marcoantoniorabr@gmail.com>
2025-11-04 21:29:53 -0300

    raças add function implemented

--- racas.py
+++ racas.py
@@ -7,7 +7,28 @@ from BovGen.db import get_db # type: ignore
 
 bp = Blueprint("racas", __name__, url_prefix= "/racas")
 
-@bp.route("/add")
+@bp.route("/add", methods=("GET", "POST"))
 @login_required
 def add():
+    db = get_db()
+    error = None
+    if request.method == "POST":
+        name = request.form["name"]
+        
+        if name is None:
+            error = "A raça precisa ter nome."
+        
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO Racas(name) VALUES (?)",
+                    (name,)
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = "A raça já existe."
+            else:
+                return redirect(url_for('menu.index'))
+    
+        flash(error)
     return render_template("app/racas/add.html")
