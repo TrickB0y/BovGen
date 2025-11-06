7b51e9a1ba04752ecd4d3cd12a77b61784606dc5
Marco Antônio R. A. B. Ribeiro <marcoantoniorabr@gmail.com>
2025-11-06 12:30:05 -0300

    readability change and edit and view page added to local blueprint

--- local.py
+++ local.py
@@ -13,12 +13,12 @@ def add():
     db = get_db()
     error = None
     if request.method == "POST":
-        name = request.form["name"]
+        nome_fazenda = request.form["nome_fazenda"]
         estado = request.form["estado"]
         cidade = request.form["cidade"]
         propietario = request.form["propietario"]
         
-        if name is None:
+        if nome_fazenda is None:
             error = "A fazenda precisa ter nome."
         if estado is None:
             error = "O estado precisa ser informado."
@@ -32,13 +32,70 @@ def add():
                 db.execute(
                     "INSERT INTO Localidades(nome_fazenda, estado, cidade, propietario)"
                     "VALUES (?, ?, ?, ?)",
-                    (name, estado, cidade, propietario)
+                    (nome_fazenda, estado, cidade, propietario)
                 )
                 db.commit()
             except db.IntegrityError:
-                error = "A fazenda já esta cadastrada."
+                error = "Existe uma fazenda com mesmo nome."
             else:
                 return redirect(url_for('menu.index'))
     
         flash(error)
     return render_template("app/local/add.html")
+
+
+@bp.route("/view", methods=("GET",))
+@login_required
+def view():
+    db = get_db()
+
+    localidades = db.execute(
+        "SELECT * FROM Localidades"
+    ).fetchall()
+    return render_template("app/local/view.html", localidades=localidades)
+
+
+@bp.route("/edit/<int:id>", methods=("GET", "POST"))
+@login_required
+def edit(id):
+    db = get_db()
+    error = None
+    
+    local = db.execute(
+        "SELECT * FROM Localidades WHERE id = ?",
+        (id,)
+    ).fetchone()
+
+    if local is None:
+        return "Local não encontrado"
+
+    if request.method == "POST":
+        nome_fazenda = request.form["nome_fazenda"]
+        estado = request.form["estado"]
+        cidade = request.form["cidade"]
+        propietario = request.form["propietario"]
+        
+        if nome_fazenda is None:
+            error = "A fazenda precisa ter nome."
+        if estado is None:
+            error = "O estado precisa ser informado."
+        if cidade is None:
+            error = "A cidade precisa ser informado."
+        if propietario is None:
+            error = "O propietario precisa ser informado."
+        
+        if error is None:
+            try:
+                db.execute(
+                    "UPDATE Localidades SET nome_fazenda = ?, estado = ?, cidade = ?, propietario = ?"
+                    "WHERE id = ?",
+                    (nome_fazenda, estado, cidade, propietario, id)
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = "Existe uma fazenda com mesmo nome."
+            else:
+                return redirect(url_for('menu.index'))
+    
+        flash(error)
+    return render_template("app/local/edit.html", local=local)
