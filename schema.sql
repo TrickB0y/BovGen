073364c10807c7ff9871d779440dede74f97c0db
Marco Antônio R. A. B. Ribeiro <marcoantoniorabr@gmail.com>
2025-11-06 02:20:03 -0300

    table localidades added and table animais fixed

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Racas;
+DROP TABLE IF EXISTS Localidades;
 DROP TABLE IF EXISTS Animais;
 
 CREATE TABLE Users (
@@ -13,10 +14,19 @@ CREATE TABLE Racas (
     name TEXT UNIQUE NOT NULL
 );
 
+CREATE TABLE Localidades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_fazenda TEXT UNIQUE NOT NULL,
+    estado TEXT NOT NULL,
+    cidade TEXT NOT NULL,
+    propietario TEXT NOT NULL
+);
+
 CREATE TABLE Animais (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     rgd TEXT UNIQUE NOT NULL,
-    raca_id INTEGER NOT NULL,
+    raca_id INTEGER,
+    nome TEXT NOT NULL,
     sexo TEXT NOT NULL,
     categoria TEXT,
     nascimento TEXT,
